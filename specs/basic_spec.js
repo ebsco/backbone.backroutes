af6eb51ec3fb05f4cd5c1ca2fc1f5a92f9ebdb1c
Ryan B Leach <rleach@ebscohost.com>
2013-07-29 16:51:51 -0400

    Adding specs

--- specs/basic_spec.js
+++ specs/basic_spec.js
@@ -1,9 +1,11 @@
 describe('Dependencies', function() {
 
+	it('Should have Underscore', function() {
+		expect(_).toBeDefined();
+	});
+
 	it('Should have Backbone', function() {
-
 		expect(Backbone).toBeDefined();
-
 	});
 
 });
