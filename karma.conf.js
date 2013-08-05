ec89ca3864616ece4c640352ba92955187657fc0
Ryan B Leach <rleach@ebscohost.com>
2013-08-05 12:32:04 -0400

    wip

--- karma.conf.js
+++ karma.conf.js
@@ -1,76 +1,74 @@
 // Karma configuration
-// Generated on Fri Aug 02 2013 11:50:44 GMT-0400 (EDT)
+// Generated on Mon Aug 05 2013 12:27:52 GMT-0400 (EDT)
+
+module.exports = function(config) {
+	config.set({
+
+		// base path, that will be used to resolve files and exclude
+		basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+		// frameworks to use
+		frameworks: ['jasmine'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'libs/jQuery/jquery.min.js',
-  'libs/underscore/underscore-min.js',
-  'libs/backbone/backbone-min.js',
-  'libs/backbone-query-parameters/backbone.queryparams.js',
-  'backbone.backroutes.js',
-  'example/scripts/app.js',
-  'specs/*.spec.js'
-];
+		// list of files / patterns to load in the browser
+		files: [
+			'libs/jQuery/jquery.min.js',
+			'libs/underscore/underscore-min.js',
+			'libs/backbone/backbone-min.js',
+			'libs/backbone-query-parameters/backbone.queryparams.js',
+			'backbone.backroutes.js',
+			'example/scripts/app.js',
+			'specs/*.spec.js'
+		],
 
-preprocessors = {
-  'backbone.backroutes.js': 'coverage'
+
+		// list of files to exclude
+		exclude: [
+
+		],
+
+
+		// test results reporter to use
+		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+		reporters: ['progress'],
+
+
+		// web server port
+		port: 9876,
+
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
+
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
+
+
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: ['Chrome'],
+
+
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout: 60000,
+
+
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false
+	});
 };
-
-// list of files to exclude
-exclude = [
-
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-// browsers = ['Chrome', 'Firefox', 'Safari'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
